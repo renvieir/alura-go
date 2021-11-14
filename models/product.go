6d36e43c1007dc67c3c8b24d15ce93eeec33a73e
Renato Pacheco Vieira <renato.pacheco.r2@gmail.com>
2021-11-14 12:25:19 -0300

    refactor: clean list product func

--- models/product.go
+++ models/product.go
@@ -23,21 +23,10 @@ func SearchAllProducts() []Produto {
 	products := []Produto{}
 
 	for allProducts.Next() {
-		var id, quantidade int
-		var nome, descricao string
-		var preco float64
-
-		err := allProducts.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		err := allProducts.Scan(&p.Id, &p.Nome, &p.Descricao, &p.Preco, &p.Quantidade)
 		if err != nil {
 			panic(err.Error())
 		}
-
-		p.Id = id
-		p.Nome = nome
-		p.Descricao = descricao
-		p.Quantidade = quantidade
-		p.Preco = preco
-
 		products = append(products, p)
 	}
 	defer conn.Close()
